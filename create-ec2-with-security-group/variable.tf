e30fbb0d499fce016b2ad8c1b9ebb92853767b0a
unusedvariable <imamsy11@gmail.com>
2023-01-27 02:20:09 +0700

    add provider config

---
+++ create-ec2-with-security-group/variable.tf
@@ -0,0 +1,13 @@
+variable "access_key" {
+  type        = string
+  description = "Access key to AWS console"
+}
+variable "secret_key" {
+  type        = string
+  description = "Secret key to AWS console"
+}
+
+variable "region" {
+  type        = string
+  description = "Region resource"
+}
